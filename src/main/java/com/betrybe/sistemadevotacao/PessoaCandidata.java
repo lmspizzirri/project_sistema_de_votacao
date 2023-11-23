6ff037a12dbd5d1f30c53756d8fd040230703082
Lucas <lm.spizzirri@gmail.com>
2023-11-22 21:53:29 -0300

    Req 3

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,17 +1,17 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * Classe PessoaCandidata.
+ * The type Pessoa candidata.
  */
-
 public class PessoaCandidata extends Pessoa {
   private int numero;
   private int votos;
+
   /**
    * Instantiates a new Pessoa candidata.
    *
-   * @param nome   the nome
    * @param numero the numero
+   * @param nome   the nome
    */
   public PessoaCandidata(int numero, String nome) {
     this.numero = numero;
@@ -19,18 +19,36 @@ public class PessoaCandidata extends Pessoa {
     this.votos = 0;
   }
 
+  /**
+   * Gets numero.
+   *
+   * @return the numero
+   */
   public int getNumero() {
     return numero;
   }
 
+  /**
+   * Gets votos.
+   *
+   * @return the votos
+   */
   public int getVotos() {
     return votos;
   }
 
+  /**
+   * Sets numero.
+   *
+   * @param numero the numero
+   */
   public void setNumero(int numero) {
     this.numero = numero;
   }
 
+  /**
+   * Receber voto.
+   */
   public void receberVoto() {
     this.votos += 1;
   }
